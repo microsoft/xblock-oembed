6e9ab437b1a0c71fdc108de888e1556c52b5e969
sushantgawali <sushant.gawali@gmail.com>
2015-12-16 18:20:37 +0530

    removed utils.py

--- oembed/filter.py
+++ oembed/filter.py
@@ -1,11 +1,13 @@
 import textwrap
-from utils import get_embed_html
 import logging
 LOG = logging.getLogger(__name__)
-
+from const import OEMBED_SERVICES
+import re
+import urllib2
+import json
 
 # Helper class to map the document URL into a form required for adding to the courseware, depending upon how it is intended to be used
-class Filter():
+class Filter:
     EMBED_CODE_TEMPLATE = textwrap.dedent("""
         <iframe
             src="{}"
@@ -21,4 +23,20 @@ class Filter():
     @staticmethod
     def get_embed_code(url):
         url = url.strip()
-        return get_embed_html(url)
+        # check if it already is an embed code
+        embed_code_regex = '<iframe '
+        matched = re.match(embed_code_regex, url, re.IGNORECASE)
+
+        if matched is not None:
+            return url
+
+        # match the url against url patterns for various services to determine the source of the document and then convert
+        # the url into an embed code depending upon whether the service supports OEmbed protocol
+        for service, urls in OEMBED_SERVICES.iteritems():
+            matched = re.match(urls['regex'], url, re.IGNORECASE)
+
+            if matched is not None:
+                res = json.load(urllib2.urlopen(urls['embed_url'].format(url)))
+                return res['html']
+
+        return Filter.EMBED_CODE_TEMPLATE.format(url)
