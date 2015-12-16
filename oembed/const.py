19a83abcaaeefc7cf7dd2b866d735e8ce7afb9ec
sushantgawali <sushant.gawali@gmail.com>
2015-12-16 17:58:19 +0530

    code cleanup for oembed filter

---
+++ oembed/const.py
@@ -0,0 +1,39 @@
+
+OEMBED_SERVICES = {
+
+    'youtube': {
+        'regex': '(https?:\/\/(www\.)?)(youtube\.com|youtu\.be|youtube\.googleapis.com)\/(?:embed\/|v\/|watch\?v=|watch\?.+&amp;v=|watch\?.+&v=)?((\w|-){11})(.*?)',
+        'embed_url': 'http://www.youtube.com/oembed?url={}&format=json'
+    },
+
+    'ted': {
+        'regex': '(https?:\/\/(www\.)?)(ted\.com)\/talks',
+        'embed_url': 'http://www.ted.com/services/v1/oembed.json?url={}'
+    },
+
+    'vimeo': {
+        'regex': 'https?:\/\/(www\.)?vimeo\.com\/',
+        'embed_url': 'https://vimeo.com/api/oembed.json?url={}'
+    },
+
+    'office_mix': {
+        'regex': '(https?:\/\/(www\.)?)(mix\.office\.com)/watch',
+        'embed_url': 'https://mix.office.com/oembed/?url={}'
+    },
+
+    'slideshare': {
+        'regex': 'https?:\/\/(www\.)?slideshare\.net',
+        'embed_url': 'http://www.slideshare.net/api/oembed/2?url={}&format=json'
+    },
+
+    'issuu': {
+        'regex': 'https?:\/\/(www\.)?issuu\.com',
+        'embed_url': 'http://issuu.com/oembed?url={}&format=json'
+    },
+
+    'soundcloud': {
+        'regex': 'https?:\/\/(www\.)?soundcloud\.com',
+        'embed_url': 'http://soundcloud.com/oembed?url={}&format=json'
+    },
+
+}
